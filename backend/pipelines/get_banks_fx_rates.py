737dec79a44b7795a7bfbfc305f0bc6ff096e2b2
yilinzyl <zhengyilin02@gmail.com>
2022-03-12 15:43:37 +0800

    changed exchange rate calculation

--- backend/pipelines/get_banks_fx_rates.py
+++ backend/pipelines/get_banks_fx_rates.py
@@ -4,9 +4,12 @@ import pandas as pd
 
 def get_banks_fx_rates(currency_from, currency_to):
     bank_functions = [get_ocbc_fx_rate, get_sc_fx_rate]
-    df = pd.DataFrame(columns=['Bank', 'Currency From', 'Currency To', 'Buying Rate', 'Selling Rate'])
+    df = pd.DataFrame(columns=['Bank', 'Currency From', 'Currency To', 'Exchange Rate'])
     for function in bank_functions:
         bank_data = function(currency_from, currency_to)
         if not bank_data.empty:
             df = pd.concat([df, bank_data], ignore_index = True)
     return df
+
+print(get_banks_fx_rates('USD', 'EUR'))
+print(get_banks_fx_rates('EUR', 'USD'))
