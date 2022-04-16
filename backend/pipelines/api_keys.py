ab73f5d98c2ab32ae03cea058082a44df94d943b
Zheng Yilin <59231608+yilinzyl@users.noreply.github.com>
2022-04-16 11:25:03 +0800

    Delete api_keys.py

--- backend/pipelines/api_keys.py
+++
@@ -1,6 +0,0 @@
-# OCBC (valid for 90 days from 12/3/22)
-OCBC_ACCESS_TOKEN = 'f5fd3f8a-785f-3af1-bec0-b19e14ab7621'
-
-# Standard Chartered
-SC_CLIENT_ID = "fc9cfdfc-db45-484e-844a-59ecc1088e6f"
-SC_CLIENT_SECRET = "6ef13d7d-057c-491d-b5c3-dfb44f098948"
